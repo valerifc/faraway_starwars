a49c531c2f23d941303ecd65fef2c51bd005da48
LANIT\Posokhov <Posokhov@lanit.ru>
2023-08-11 16:18:05 +0500

    feature hero types & constants

---
+++ src/features/heroes/constants.ts
@@ -0,0 +1,20 @@
+import { Hero } from "./types";
+
+export const heroEmpty: Hero = {
+  name: "",
+  height: "",
+  mass: "",
+  hair_color: "",
+  skin_color: "",
+  eye_color: "",
+  birth_year: "",
+  gender: "",
+  homeworld: "",
+  films: [],
+  species: [],
+  vehicles: [],
+  starships: [],
+  created: "",
+  edited: "",
+  url: "",
+};
---
+++ src/features/heroes/types.ts
@@ -0,0 +1,24 @@
+export type Hero = {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export type HeroesState = {
+  heroes: Hero[];
+  heroesDiff: Map<string, string | string[]>;
+  hero: Hero;
+};
