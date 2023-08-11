53ced7a06ba9faa69f281a13608cd60211bad07a
LANIT\Posokhov <Posokhov@lanit.ru>
2023-08-11 18:08:09 +0500

    redirect to concrete hero

--- src/pages/HeroesPage/HeroesPage.tsx
+++ src/pages/HeroesPage/HeroesPage.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
+import { webRoutes } from "../../constants/webRoutes";
+import { useNavigate } from "react-router-dom";
 import { fetchHeroes, selectHeroes } from "../../features/heroes/heroesSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { Box, Stack, Typography } from "@mui/material";
 
 const HeroesPage = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const heroes = useAppSelector(selectHeroes);
 
   useEffect(() => {
@@ -14,14 +17,23 @@ const HeroesPage = () => {
 
   return (
     <>
-      <Typography variant="h4" sx={{ m: 2 }}>
+      {/* <div className="StarWars">TEST</div> */}
+      <Typography variant="h4" sx={{ m: 2 }} className="StarWars">
         Список персонажей
       </Typography>
       <Stack>
-        {(heroes?.results || []).map(({ name }, index) => {
+        {(heroes?.results || []).map(({ name, url }, index) => {
           return (
             <Box sx={{ m: 2 }} key={index}>
-              <Typography variant="body1">{name}</Typography>
+              <Typography
+                variant="body1"
+                sx={{ cursor: "pointer" }}
+                onClick={() => {
+                  navigate(`/${webRoutes.heroes}/${url.split("/")[5]}`); // TODO: via regex
+                }}
+              >
+                {name}
+              </Typography>
             </Box>
           );
         })}
