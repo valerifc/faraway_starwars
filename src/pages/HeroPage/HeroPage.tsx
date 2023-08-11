b4a680c3fc54859efb05006b4ba55015ee84f1e3
LANIT\Posokhov <Posokhov@lanit.ru>
2023-08-11 16:15:55 +0500

    init data receiving

--- src/pages/HeroPage/HeroPage.tsx
+++ src/pages/HeroPage/HeroPage.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const HeroPage = () => {
+  const [hero, setHero] = useState<any>({});
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/${heroId}`).then((res: any) => {
+      console.log(res);
+      setHero(res?.data || []);
+    });
+  }, []);
   const { id: heroId } = useParams();
   return (
-    <h1>Cтраница с подробной информацией по выбранному персонажу /:{heroId}</h1>
+    <>
+      <h1>
+        Cтраница с подробной информацией по выбранному персонажу /:{heroId}
+      </h1>
+      <>
+        {Object.entries(hero)?.map((pair: any) => {
+          const [key, value] = pair;
+          return (
+            <p>
+              {key}: {value}
+            </p>
+          );
+        })}
+      </>
+    </>
   );
 };
 
--- src/pages/HeroesPage/HeroesPage.tsx
+++ src/pages/HeroesPage/HeroesPage.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const HeroesPage = () => (
-  <h1>Главная страница, где нужно отобразить список или карточки персонажей</h1>
-);
+const HeroesPage = () => {
+  const [heroes, setHeroes] = useState<any>([]);
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people").then((res: any) => {
+      console.log(res);
+      setHeroes(res?.data?.results || []);
+    });
+  }, []);
+  return (
+    <>
+      <h1>
+        Главная страница, где нужно отобразить список или карточки персонажей
+      </h1>
+      {heroes?.map((hero: any) => {
+        const { name } = hero;
+        return (
+          <>
+            <h5>{name}</h5>
+            <br />
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default HeroesPage;
